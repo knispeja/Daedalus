ac2064191aeef07c36c9c25f6ac852bc080e642a
Jacob Knispel <knispeja@rose-hulman.edu>
2016-11-12 00:11:50 -0500

    Joined questions list and Q&A HTML

--- js/mythquestions.js
+++ js/mythquestions.js
@@ -1,13 +1,32 @@
+// Constants
 var TRUE = 0;
 var FALSE = 1;
 var TRUE_FALSE_OPTIONS = ['True', 'False'];
 var MULTIPLE_CHOICE_START_CODE = 65; // capital a:'A'
+var TOTAL_QUESTIONS_ASKED = 10;
+
+// Globals
+var questionElement;
+var optionElements = [];
+var questionsLeft = TOTAL_QUESTIONS_ASKED;
 
 var Question = function(text, options, answer, explanation) {
     this.text = text;
     this.options = options;
     this.answer = answer;
     this.explanation = explanation;
+
+    this.display = function() {
+        questionElement.innerHTML = this.text;
+        for(var i=0; i<optionElements.length; i++) {
+            if (i < options.length) {
+                optionElements[i].innerHTML = this.options[i];
+                optionElements[i].style.display = "block";
+            } else {
+                optionElements[i].style.display = "none";
+            }
+        }
+    }
 }
 
 function makeTrueFalseQuestion(question, answer, explanation) {
@@ -32,8 +51,8 @@ function makeMultipleChoiceQuestion(question, options, explanation="", answer=0)
     }
 
     // Append letters to the beginning of the multiple choice questions
-    var code = MULTIPLE_CHOICE_START_CODE;
-    options = options.map(function(opt) {return String.fromCharCode(code++) + ". " + opt});
+    // var code = MULTIPLE_CHOICE_START_CODE;
+    // options = options.map(function(opt) {return String.fromCharCode(code++) + ". " + opt});
 
     return new Question(
         question,
@@ -66,9 +85,31 @@ var QUESTIONS = [
     )
 ];
 
+function displayRandomQuestion() {
+    if(questionsLeft-- == 0) {
+        showCanvas();
+        return;
+    }
+
+    var i = randomIndexOf(QUESTIONS);
+    var question = QUESTIONS[i];
+    QUESTIONS.splice(i, 1); // remove question from list
+    question.display();
+}
+
 function init() {
     mazeGenInit();
     hideCanvas();
+
+    questionElement = document.getElementById("question");
+
+    optionElements.push(document.getElementById("A"));
+    optionElements.push(document.getElementById("B"));
+    optionElements.push(document.getElementById("C"));
+    optionElements.push(document.getElementById("D"));
+    optionElements.push(document.getElementById("E"));
+
+    displayRandomQuestion();
 }
 
 window.onload = init;
